d6be3fa5ff336ec6531c3ac29179e387beeaacf2
Sanchit <wsanchit@gmail.com>
2023-12-23 16:37:17 +0000

    update java file

--- Communication Skills/System Design/Test.java
+++ Communication Skills/System Design/Test.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Test {
 
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+
+        list.forEach(t -> {
+            System.out.println(t);
+        });
+        
+    }
 }
